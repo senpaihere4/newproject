f2530c7b9ff33e368ef49f1fa651b94ac3a730f7
senpaihere4 <senpaihere4@gmail.com>
2023-09-27 22:41:36 +0700

    new

---
+++ src/main/java/com/wakanda/blackking/BlackkingApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda.blackking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wakanda/blackking/BlackkingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakanda.blackking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
